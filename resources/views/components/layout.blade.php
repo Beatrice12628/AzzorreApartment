5b7a614d2b11ab9403ff2f90a5627d5245618270
Beatrice Rozza <tricerozza@gmail.com>
2025-11-25 14:57:47 +0100

    introduzione lingue

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? config('app.name') }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script type="text/javascript">
+    function googleTranslateElementInit() {
+        new google.translate.TranslateElement(
+            {pageLanguage: 'it'},
+            'google_translate_element'
+        );
+    }
+</script>
+
+<script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
 </head>
 <body class="d-flex flex-column min-vh-100">
     <x-navbar />
